0d02ba6bca0f391c7711b2a587c05ca69455ec54
Jason Ho <jasonho353@gmail.com>
2019-10-25 17:55:26 -0500

    new resume

--- home.js
+++ home.js
@@ -8,9 +8,19 @@ var list = new Vue({
   el: '#list',
   data: {
     items: [
+      {message: 'Contact', url: ''},
       {message: 'Github', url: 'https://github.com/cod1r'},
       {message: 'LinkedIn', url: 'https://www.linkedin.com/in/jason-ho-uh/'},
       {message: 'Resume', url: 'Jason Ho RESUME.pdf'}
+    ]
+  }
+})
+Vue.component('social-media',{
+  data: {
+    items: [
+      {item: 'Twitter', url: 'https://twitter.com/cod1r'},
+      {item: 'Facebook', url: 'https://www.facebook.com/profile.php?id=100041088257875'},
+      {item: 'Instagram', url: 'https://www.instagram.com/theuhcod1r/'}
     ]
   }
 })
