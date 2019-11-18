ffc4ae8d2a73539d3c85c01f8d908edbe9ad7097
cod1r <jasonho353@gmail.com>
2019-11-18 15:43:25 -0600

    learning to use express

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express')
 var app = express()
-const port = 3000
+const port = 80
 
-// respond with "hello world" when a GET request is made to the homepage
-app.post('/input', function (req, res) {
-  res.send('hello world')
+app.use('index2.html', function (req, res) {
+  console.log("HELLO")
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
