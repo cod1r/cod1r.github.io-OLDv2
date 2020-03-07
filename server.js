c5b3e4ea8f62dae42816132c9020b7b046b9598d
cod1r <jasonho353@gmail.com>
2020-03-07 17:21:48 -0600

    updating resume

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-const port = 80
-
-app.use('index2.html', function (req, res) {
-  console.log("HELLO")
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
